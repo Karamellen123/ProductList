2f7cc650b63ed9b376842733dd1b0614d0e9f41d
Karamellen <94791485+Karamellen123@users.noreply.github.com>
2024-02-14 14:43:06 +0100

    Update Program.cs
    Added the code directly to github online

--- ProductList/Program.cs
+++ ProductList/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+List<string> ProductLists = new List<string>();
+
+MainMenu();
+
+void printList()
+{
+    Console.WriteLine("Printing list");
+    for (int i = 0; i < ProductLists.Count; i++)
+    {
+        Console.WriteLine("Item: " + ProductLists[i]);
+    }
+        
+}
+
+void AddProduct()
+{
+    Console.WriteLine("What product would you like to add? ");
+    string temp = Console.ReadLine();
+
+    if (temp == "exit")
+        printList();
+    else
+    {
+        ProductLists.Add(temp);
+        Console.WriteLine("Temp: " + temp);
+        MainMenu();
+    }
+}
+
+void MainMenu()
+{
+    Console.WriteLine("temp");
+    Console.WriteLine("temp");
+    AddProduct();
+}
